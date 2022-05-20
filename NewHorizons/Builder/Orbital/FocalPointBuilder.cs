725333a5bcf9bc776a10245b5a0e9e64f6da0b37
Noah Pilarski <mehmehmeh313@gmail.com>
2022-05-20 00:08:38 -0400

    Trinary Builder

--- NewHorizons/Builder/Orbital/FocalPointBuilder.cs
+++ NewHorizons/Builder/Orbital/FocalPointBuilder.cs
@@ -14,15 +14,18 @@ namespace NewHorizons.Builder.Orbital
         {
             var module = config.FocalPoint;
 
-            var binary = go.AddComponent<BinaryFocalPoint>();
+            bool isTrinary = !string.IsNullOrWhiteSpace(module.Tertiary);
+            var binary = isTrinary ? go.AddComponent<TrinaryFocalPoint>() : go.AddComponent<BinaryFocalPoint>();
             binary.PrimaryName = module.Primary;
             binary.SecondaryName = module.Secondary;
+            if (isTrinary) ((TrinaryFocalPoint)binary).TertiaryName = module.Tertiary;
 
             // Below is the stupid fix for making circumbinary planets or wtv
 
             // Grab the bodies from the main dictionary
             NewHorizonsBody primary = null;
             NewHorizonsBody secondary = null;
+            NewHorizonsBody tertiary = null;
             foreach (var body in Main.BodyDict[Main.Instance.CurrentStarSystem])
             {
                 if (body.Config.Name == module.Primary)
@@ -33,16 +36,31 @@ namespace NewHorizons.Builder.Orbital
                 {
                     secondary = body;
                 }
+                else if (isTrinary && body.Config.Name == module.Tertiary)
+                {
+                    tertiary = body;
+                }
                 if (primary != null && secondary != null)
                 {
-                    break;
+                    if ((isTrinary && tertiary != null) || !isTrinary) break;
                 }
             }
 
-            if (primary == null || secondary == null)
+            if (isTrinary)
             {
-                Logger.LogError($"Couldn't make focal point between [{module.Primary} = {primary}] and [{module.Secondary} = {secondary}]");
-                return;
+                if (primary == null || secondary == null || tertiary == null)
+                {
+                    Logger.LogError($"Couldn't make focal point between {module.Primary} and {module.Secondary} and {module.Tertiary}");
+                    return;
+                }
+            }
+            else
+            {
+                if (primary == null || secondary == null)
+                {
+                    Logger.LogError($"Couldn't make focal point between [{module.Primary} = {primary}] and [{module.Secondary} = {secondary}]");
+                    return;
+                }
             }
 
             var gravitationalMass = GetGravitationalMass(primary.Config) + GetGravitationalMass(secondary.Config);
@@ -63,6 +81,31 @@ namespace NewHorizons.Builder.Orbital
 
             fakeMassConfig.Orbit = new OrbitModule();
             fakeMassConfig.Orbit.CopyPropertiesFrom(config.Orbit);
+
+            if (isTrinary)
+            {
+                var secondGravitationalMass = gravitationalMass + GetGravitationalMass(tertiary.Config);
+                // Copying it because I don't want to modify the actual config
+                var fakeMassConfig2 = new PlanetConfig();
+
+                // Now need to fake the 3 values to make it return this mass
+                fakeMassConfig2.Base.SurfaceSize = 1;
+                fakeMassConfig2.Base.SurfaceGravity = secondGravitationalMass * GravityVolume.GRAVITATIONAL_CONSTANT;
+                fakeMassConfig2.Base.GravityFallOff = primary.Config.Base.GravityFallOff;
+
+                // Other stuff to make the fake barycenter not interact with anything in any way
+                fakeMassConfig2.Name = config.Name + "_FakeBarycenterMass2";
+                fakeMassConfig2.Base.SphereOfInfluence = 0;
+                fakeMassConfig2.Base.HasMapMarker = false;
+                fakeMassConfig2.Base.HasReferenceFrame = false;
+
+                fakeMassConfig2.Orbit = new OrbitModule();
+                fakeMassConfig2.Orbit.CopyPropertiesFrom(config.Orbit);
+
+                fakeMassConfig.Orbit.PrimaryBody = fakeMassConfig2.Name;
+
+                ((TrinaryFocalPoint)binary).FakeMassBody2 = PlanetCreationHandler.GenerateBody(new NewHorizonsBody(fakeMassConfig2, mod));
+            }
 
             binary.FakeMassBody = PlanetCreationHandler.GenerateBody(new NewHorizonsBody(fakeMassConfig, mod));
         }

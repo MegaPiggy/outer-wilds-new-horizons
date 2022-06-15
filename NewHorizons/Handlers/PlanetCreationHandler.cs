d3282feb062329ba9b30c34c7812ba5cc2b5db2c
Noah Pilarski <mehmehmeh313@gmail.com>
2022-06-15 11:01:38 -0400

    No more focal point sector and volumes

--- NewHorizons/Handlers/PlanetCreationHandler.cs
+++ NewHorizons/Handlers/PlanetCreationHandler.cs
@@ -313,7 +313,10 @@ namespace NewHorizons.Handlers
                 MarkerBuilder.Make(go, body.Config.name, body.Config);
             }
 
-            VolumesBuilder.Make(go, owRigidBody, body.Config, sphereOfInfluence);
+            if (body.Config.FocalPoint == null)
+            {
+                VolumesBuilder.Make(go, owRigidBody, body.Config, sphereOfInfluence);
+            }
 
             if (body.Config.FocalPoint != null)
             {
@@ -322,6 +325,9 @@ namespace NewHorizons.Handlers
 
             // Do stuff that's shared between generating new planets and updating old ones
             go = SharedGenerateBody(body, go, sector, owRigidBody);
+
+            if (sector.transform.childCount == 0)
+                GameObject.Destroy(sector.gameObject);
 
             body.Object = go;
 
